8111bb2b504e1a3021e6428dad8ea4e00b739526
dsoz34 <sofiane.antibes@gmail.com>
2023-07-03 09:17:32 +0200

    modification du composant app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD1';
+
+  nom:String="Jean"
+  verif:boolean=true
+  element:number=1
+
+  etudiants:any[]=[
+    {"nom":"Dupont", "prenom":"Jean",    "age":25},
+    {"nom":"lalu",   "prenom":"lolo",    "age":12},
+    {"nom":"gngn",   "prenom":"trtr",    "age":112},
+    {"nom":"nom",   "prenom":"prenom",    "age":17},
+    {"nom":"berran", "prenom":"bernard", "age":85}
+  ]
+
+
+  getNomClass(premier:boolean, dernier:boolean, pair:boolean){
+    var nomClass="";
+    if(premier || dernier){
+      nomClass+= "gras";
+    }
+    if(pair){
+      nomClass+= " bleu"
+    } else {
+      nomClass+= " rouge"
+    }
+
+
+    return nomClass;
+    
+  }
+
+
+
 }
